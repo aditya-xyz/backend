46732e0f8538228aaf730ec26e616faa5839ab0c
Aditya Patil <aditya.patil.gverse@gmail.com>
2023-01-05 17:19:26 -0700

    added sort and limit to returned values

--- controllers/experiences.js
+++ controllers/experiences.js
@@ -2,7 +2,7 @@ import Experience from "../models/Experience.js"
 
 export const getExperiences = async (req, res) => {
   try {
-    const experience = await Experience.find()
+    const experience = await Experience.find().sort({ id: 1 }).limit(9)
     res.status(200).json(experience)
   } catch (err) {
     res.status(404).json({ message: err.message })
--- controllers/projects.js
+++ controllers/projects.js
@@ -2,7 +2,7 @@ import Project from "../models/Project.js"
 
 export const getProjects = async (req, res) => {
   try {
-    const project = await Project.find()
+    const project = await Project.find().sort({ id: 1 }).limit(8)
     res.status(200).json(project)
   } catch (err) {
     res.status(404).json({ message: err.message })
